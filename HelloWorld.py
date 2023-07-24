83b1c567071c22212804e07fc4169e6d85f66e7d
Dipu Singha <hunabopa@gmail.com>
2023-07-24 15:50:11 +0600

    Inheritance in Python
    Sure! Inheritance is a fundamental concept in object-oriented programming (OOP) and is supported in Python. It allows you to create a new class (called the "child" class) that inherits the properties and behaviors of an existing class (called the "parent" class). The child class can then extend or modify the functionality of the parent class while reusing its code.
    
    Inheritance is useful for promoting code reusability, as it enables you to create specialized classes that share common attributes and methods from a base class.
    
    Here's an example of how you can use inheritance in Python for your GitHub repository:
    
    Let's assume you have a base class called `Repository`, which represents a generic GitHub repository with basic properties like `name`, `description`, `created_date`, and methods to interact with the repository.
    
    ```python
    class Repository:
        def __init__(self, name, description, created_date):
            self.name = name
            self.description = description
            self.created_date = created_date
    
        def clone(self):
            print(f"Cloning {self.name} repository...")
    
        def delete(self):
            print(f"Deleting {self.name} repository...")
    ```
    
    Now, let's say you want to create a specialized class called `PrivateRepository` that inherits from the `Repository` class but also has additional properties and methods related to private repositories.
    
    ```python
    class PrivateRepository(Repository):
        def __init__(self, name, description, created_date, access_level='private'):
            super().__init__(name, description, created_date)
            self.access_level = access_level
    
        def grant_access(self, user):
            print(f"Granting {user} access to {self.name} repository...")
    
        def revoke_access(self, user):
            print(f"Revoking {user} access from {self.name} repository...")
    ```
    
    In this example, the `PrivateRepository` class inherits from the `Repository` class by including it in the parentheses of the class declaration. The `super()` function is used in the `__init__` method to call the constructor of the parent class, ensuring that the properties from the base class are also initialized.
    
    Now, you can create instances of both classes and use their respective methods:
    
    ```python
    # Creating a generic repository
    generic_repo = Repository("example_repo", "This is a generic repository", "2023-07-24")
    generic_repo.clone()
    generic_repo.delete()
    
    # Creating a private repository
    private_repo = PrivateRepository("private_example", "This is a private repository", "2023-07-24")
    private_repo.clone()
    private_repo.grant_access("user123")
    private_repo.revoke_access("user456")
    ```
    
    By using inheritance, you can easily create specialized classes that extend the functionality of the base class while keeping the code organized and promoting reusability in your GitHub repository project.

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,12 +1,75 @@
-# Python Object Oriented Programming in Python
-from car import Car
 
-#car_1 = Car("Chevy","Corvette",2023,"blue")
-#car_2 =Car("Ford","Mustang",2022,"red")
+# Python inheritance
+class Animal:
+    alive = True
 
-#car_1.wheels = 2
+    def eat(self):
+        print("This animal is eating.")
+    def sleep(self):
+        print("This animal is sleeping.")
 
-#print(car_1.wheels)
-#print(car_2.wheels)
-print(Car.wheels)
+class Rabbit(Animal):
+    def run(self):
+        print("This Rabbit is running. ")
+class Fish(Animal):
+    def swim(self):
+        print("This fish is swimming.")
+class Hawk(Animal):
+    def fly(self):
+        print("This hawk is flying.")
+rabbit = Rabbit()
+fish = Fish()
+hawk = Hawk()
+
+#print(rabbit.alive)
+#fish.eat()
+#hawk.sleep()
+rabbit.run()
+fish.swim()
+hawk.fly()
+
+# Python Multilevel Inheritance = when a derived (child) class inherits another derived(child) class
+class Organism():
+    alive = True
+
+class Animal(Organism):
+    def eat(self):
+        print("This animal is eating.")
+
+class Dog(Animal):
+    def bark(self):
+        print("This dog is barking.")
+dog = Dog()
+print(dog.alive) # inherited form the Organism class
+dog.eat()        # inherited from the Animal class
+dog.bark()       # defined in dog class  
+
+# Muitiple inheritance = when a child class is derived from more than one parent class
+
+class Prey():
+
+    def flee(self):
+        print("This animal flees")
+
+class Predator():
+
+    def hunt(self):
+        print("This animal is hunting.")
+
+class Rabbit(Prey):
+    pass
+class Hawk(Predator):
+    pass
+class Fish(Prey, Predator):
+    pass
+
+rabbit = Rabbit()
+hawk = Hawk()
+fish = Fish()
+
+#rabbit.flee()
+#hawk.hunt()
+fish.flee()
+fish.hunt()
+
 
