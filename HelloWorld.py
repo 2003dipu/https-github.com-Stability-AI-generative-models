e989f954f857de3b8596275a3dbef434feb2a548
Dipu Singha <hunabopa@gmail.com>
2023-07-25 21:45:59 +0600

    How about "Learn how to use  objects as arguments of different types in Python functions"
    This repo contains a collection of Python scripts that demonstrate how to use objects as arguments in functions. You will learn how to pass objects of different types, such as strings, lists, dictionaries, and custom classes, and how to modify them inside the functions. You will also learn how to return objects from functions, and how to handle errors and exceptions when working with objects. This repo is intended for beginners who want to improve their understanding of object-oriented programming in Python.

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,70 +1,26 @@
-""" Python Method overriding = is the ability of an OOP language to allow a subclass aka a Child Class
- to provide a specific implementation of a method that is already provided by one of its parents. In this case 
- we are going to override the eat method.
- Basically speaking an object will use a method that is more closely associated with itself first before relying
- on a method that it may inherit from a parent class"""
-class Animal:
-    def eat(self):
-        print("This animal is eating")
-class Rabbit(Animal):
-    def eat(self):
-        print("This rabbit is eating a carrot.")
-rabbit = Rabbit()
-rabbit.eat()
-
-# Method chaining = calling multiple methods sequentially ,
-#  each call performs an action on the same object and returns self.
 class Car:
-    def turn_on(self):
-        print("You turn on the engine.")
-        return self
-    def drive(self):
-        print("You drive the car.")
-        return self
-    def brake(self):
-        print("You step on the braakes.")
-        return self
-    def turn_off(self):
-        print("You turn off the engine.")
-        return self
-car = Car()
-car.turn_on()\
-    .drive()\
-    .brake()\
-    .turn_off()
+    color = None
 
-# Super () = super functions are used to give access to the methods of a parent class.
-# returns a temporary object of a parent class when used.
-class Rectangle():
-        
-        def __init__(self,length,width):
-            self.length = length
-            self.width = width
-class Square(Rectangle):
-
-    def __init__(self,length,width):
-        super().__init__(length,width)
-    def area(self):
-         return self.length*self.width
-    
-class Cube(Rectangle):
-
-    def __init__(self,length,width,height):
-        super().__init__(length,width)
-        self.height = height
-    def volume(self):
-         return self.length*self.width*self.height
-
-square = Square(3,3)
-cube = Cube(4,4,4)
-print(square.area())
-print(cube.volume())
-
-# THis is an example of speech syunthesis in English.
+class Motorcycle:
+    color = None
 
 
+def change_color(vehicle,color):
 
+    vehicle.color = color
 
+car_1 = Car()
+car_2 = Car()
+car_3 = Car()
 
+bike_1 = Motorcycle()
 
-motorcycle.go()
+change_color(car_1,"red")
+change_color(car_2,"white")
+change_color(car_3,"blue")
+change_color(bike_1, "black")
+
+print(car_1.color)
+print(car_2.color)
+print(car_3.color)
+print(bike_1.color)
