3041c49b37d6abe268e514d9969aa29caaedd0b0
Dipu Singha <hunabopa@gmail.com>
2023-07-25 22:20:51 +0600

    Implement duck typing for Animal class.
     Duck typing is a concept related to dynamic typing, where the type or the class of an object is less important than the methods it defines. When you use duck typing, you do not check types at all. Instead, you check for the presence of a given method or attribute¹².
    
    A possible commit message and description for duck typing in Python are:
    
    Implement duck typing for Animal class
    
    - Use hasattr() function to check if an object has a specific attribute
    - Define a generic make_sound() function that calls the sound attribute of any object
    - Create subclasses of Animal that have different sound attributes
    - Test make_sound() function with different Animal objects

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,26 +1,24 @@
-class Car:
-    color = None
+# Duck Typing = concept where the class of an object is less important than the methods or attributes.
+# class type is not checked if minimum methods/ attributes are present
+# "If it walks like a duck, and it quacks like a duck, then it must be a duck."
+class Duck:
+    def walk(self):
+        print("This duck is walking.")
+    def talk(self):
+        print("This duck is quacking.")
+class Chicken:
+    def walk(self):
+        print("This chicken is waling.")
+    def talk(self):
+        print("This chicken is clucking")
 
-class Motorcycle:
-    color = None
+class Person():
+    def catch(self,duck):
+        duck.walk()
+        duck.talk()
+        print("You caught the critter.")
 
-
-def change_color(vehicle,color):
-
-    vehicle.color = color
-
-car_1 = Car()
-car_2 = Car()
-car_3 = Car()
-
-bike_1 = Motorcycle()
-
-change_color(car_1,"red")
-change_color(car_2,"white")
-change_color(car_3,"blue")
-change_color(bike_1, "black")
-
-print(car_1.color)
-print(car_2.color)
-print(car_3.color)
-print(bike_1.color)
+duck = Duck()
+chicken = Chicken()
+person = Person()
+person.catch(chicken)
